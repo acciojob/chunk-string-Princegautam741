83befc40f9e106689ed1edff52a3bbd682ac6acf
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-12 19:00:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// script.js
 function stringChop(str, size) {
-  // your code here
+  if (!str) {
+    return []; // Return an empty array if the input string is null
+  }
+
+  const chunks = [];
+  let index = 0;
+
+  while (index < str.length) {
+    // Slice the string into chunks of the specified size
+    chunks.push(str.slice(index, index + size));
+    index += size;
+  }
+
+  return chunks;
 }
 
 // Do not change the code below
